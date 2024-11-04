4baf547ee5c75a8866c4fe71332b8fb56c8a4399
Ddululu <164765837+Ddululu@users.noreply.github.com>
2024-11-04 17:08:29 +0900

    전체 Entity 초기 구성

---
+++ backend/src/main/java/com/sesac/backend/entity/Department.java
@@ -0,0 +1,23 @@
+package com.sesac.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Department {
+
+    @Id
+    @GeneratedValue
+    private UUID departmentId; // 학과ID
+
+    @Column(nullable = false)
+    private String departmentName; // 학과명
+}
