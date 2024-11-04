5fe99c74bd7413f271eb6ffdd6a92931eefd8d5b
git-dongjin <y2kf9a0q@gmail.com>
2024-11-04 15:07:26 +0900

    feat: Assignments 최초 커밋
    과제 생성/제출/채점 기능 구현
    
    resolves: #20

---
+++ backend/src/main/java/com/sesac/backend/assignment/dto/AssignmentsDto.java
@@ -0,0 +1,14 @@
+package com.sesac.backend.assignment.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class AssignmentsDto {
+    private long id;
+
+    // TODO
+}
---
+++ backend/src/main/java/com/sesac/backend/assignment/service/AssignmentsService.java
@@ -0,0 +1,8 @@
+package com.sesac.backend.assignment.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AssignmentsService {
+    // TODO
+}
