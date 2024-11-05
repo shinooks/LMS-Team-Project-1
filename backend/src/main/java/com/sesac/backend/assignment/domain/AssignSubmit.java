0a405b0626d2ea17e42041024372b53cf0e5fb01
git-dongjin <y2kf9a0q@gmail.com>
2024-11-05 14:13:26 +0900

    feat: AssignSubmit 구현
    AssignSubmit 구현
    
    resolve: #20

--- backend/src/main/java/com/sesac/backend/assignment/domain/AssignSubmit.java
+++ backend/src/main/java/com/sesac/backend/assignment/domain/AssignSubmit.java
@@ -1,6 +1,9 @@
 package com.sesac.backend.assignment.domain;
 
+import com.sesac.backend.entity.Assignment;
+import com.sesac.backend.entity.Student;
 import jakarta.persistence.*;
+import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.*;
 
@@ -15,6 +18,13 @@ public class AssignSubmit {
     @Id
     @GeneratedValue
     private UUID assignSubmitId;
-
-    // TODO
+    @ManyToOne
+    @JoinColumn(name = "assignId", nullable = false)
+    private Assignment assignment;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "studentId", nullable = false)
+    private Student student;
+    private String answer;
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime submitAt = LocalDateTime.now(); // 제출일시
 }
