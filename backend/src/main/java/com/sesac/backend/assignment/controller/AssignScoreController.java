619a1e14499eb3d4e0ad978422ff0d63b4f58cfe
git-dongjin <y2kf9a0q@gmail.com>
2024-11-05 12:31:50 +0900

    feat: AssignScoreController 구현
    AssignScoreController CRUD
    
    resolves: #20

--- backend/src/main/java/com/sesac/backend/assignment/controller/AssignScoreController.java
+++ backend/src/main/java/com/sesac/backend/assignment/controller/AssignScoreController.java
@@ -1,13 +1,16 @@
 package com.sesac.backend.assignment.controller;
 
+import com.sesac.backend.assignment.dto.AssignScoreDto;
 import com.sesac.backend.assignment.service.AssignScoreService;
+import java.util.List;
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @CrossOrigin("*")
+@RequestMapping("/assignments/score")
 @RestController
 public class AssignScoreController {
 
@@ -18,5 +21,31 @@ public class AssignScoreController {
         this.assignScoreService = assignScoreService;
     }
 
-    // TODO
+    @GetMapping("/{id}")
+    public AssignScoreDto getScore(@PathVariable("id") UUID assignScoreId) {
+        return assignScoreService.findById(assignScoreId);
+    }
+
+    @GetMapping("")
+    public List<AssignScoreDto> getAllScores() {
+        return assignScoreService.findAll();
+    }
+
+    @PostMapping("")
+    public AssignScoreDto addScore(AssignScoreDto assignScoreDto) {
+        return assignScoreService.save(assignScoreDto);
+    }
+
+    @PutMapping("")
+    public AssignScoreDto updateScore(AssignScoreDto assignScoreDto) {
+        AssignScoreDto saved = assignScoreService.findById(assignScoreDto.getAssignScoreId());
+        saved.setScore(assignScoreDto.getScore());
+        saved.setVisibility(assignScoreDto.getVisibility());
+        return assignScoreService.save(saved);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteScore(@PathVariable("id") UUID assignScoreId) {
+        assignScoreService.delete(assignScoreId);
+    }
 }
