50ad5aa3659d2e0f1712dbf826dc2fb80b272335
git-dongjin <y2kf9a0q@gmail.com>
2024-11-05 11:58:21 +0900

    feat: AssignScoreDto 구현
    AssignScore Dto 구현
    
    resolves: #20

--- backend/src/main/java/com/sesac/backend/entity/AssignScore.java
+++ backend/src/main/java/com/sesac/backend/entity/AssignScore.java
@@ -1,13 +1,6 @@
 package com.sesac.backend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import java.util.UUID;
 import lombok.*;
 
@@ -28,5 +21,13 @@ public class AssignScore {
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "studentId", nullable = false)
     private Student student;
+    @Column(nullable = false)
     private int score;
+    @Enumerated
+    @Column(nullable = false)
+    private Visibility visibility;
+
+    public enum Visibility {
+        PUBLIC, PRIVATE
+    }
 }
---
+++ backend/src/main/java/com/sesac/backend/user/dto/StudentDto.java
@@ -0,0 +1,14 @@
+package com.sesac.backend.user.dto;
+
+import java.util.*;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentDto {
+
+    private UUID studentId;
+}
