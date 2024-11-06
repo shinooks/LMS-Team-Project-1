861f4a53cdfd7819784152fb73601015c3320b6f
git-dongjin <y2kf9a0q@gmail.com>
2024-11-07 00:25:47 +0900

    feat: FinalExamDto 구현
    FinalExamDto 구현
    
    resolves: #20

--- backend/src/main/java/com/sesac/backend/assignment/dto/FinalExamDto.java
+++ backend/src/main/java/com/sesac/backend/assignment/dto/FinalExamDto.java
@@ -1,5 +1,18 @@
 package com.sesac.backend.assignment.dto;
 
-public class FinalExamDto {
+import com.sesac.backend.entity.Course;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.*;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class FinalExamDto {
+    private UUID finalExamId;
+    private Course course;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 }
