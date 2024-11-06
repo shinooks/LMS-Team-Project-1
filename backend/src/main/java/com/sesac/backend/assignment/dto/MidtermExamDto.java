46d4b792bfce3f7d692ae64952d389d414ea2f7d
git-dongjin <y2kf9a0q@gmail.com>
2024-11-07 01:55:39 +0900

    feat: MidtermExamDto 구현
    MidtermExamDto 구현
    
    resolves: #20

--- backend/src/main/java/com/sesac/backend/assignment/dto/MidtermExamDto.java
+++ backend/src/main/java/com/sesac/backend/assignment/dto/MidtermExamDto.java
@@ -1,5 +1,18 @@
 package com.sesac.backend.assignment.dto;
 
-public class MidtermExamDto {
+import com.sesac.backend.entity.Course;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.*;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class MidtermExamDto {
+    private UUID midtermExamId;
+    private Course course;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 }
