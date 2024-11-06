1be74540fd6a3ee02f6a08029bf3fb7d0d2defcd
git-dongjin <y2kf9a0q@gmail.com>
2024-11-07 00:23:16 +0900

    feat: FinalExam 엔티티 생성
    FinalExam 엔티티 생성
    
    resolves: #20

--- backend/src/main/java/com/sesac/backend/assignment/domain/FinalExam.java
+++ backend/src/main/java/com/sesac/backend/assignment/domain/FinalExam.java
@@ -1,5 +1,24 @@
 package com.sesac.backend.assignment.domain;
 
-public class FinalExam {
+import com.sesac.backend.entity.Course;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.*;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class FinalExam {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID finalExamId;
+    @OneToOne
+    @JoinColumn(name = "courseId", nullable = false)
+    private Course course;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
 }
