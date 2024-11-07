bc27646697cb580359be95d67edbfcc471856a49
git-dongjin <y2kf9a0q@gmail.com>
2024-11-07 11:20:14 +0900

    feat: MidtermExamScore 구현
    MidtermExamScore 구현
    
    resolves: #20

--- backend/src/main/java/com/sesac/backend/assignment/domain/MidtermExamScore.java
+++ backend/src/main/java/com/sesac/backend/assignment/domain/MidtermExamScore.java
@@ -1,5 +1,33 @@
 package com.sesac.backend.assignment.domain;
 
-public class MidtermExamScore {
+import com.sesac.backend.entity.Student;
+import jakarta.persistence.*;
+import java.util.UUID;
+import lombok.*;
 
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class MidtermExamScore {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID midtermExamScoreId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "midtermExamId", nullable = false)
+    private MidtermExam midtermExam;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "studentId", nullable = false)
+    private Student student;
+    @Column(nullable = false)
+    private int score;
+    @Enumerated
+    @Column(nullable = false)
+    private Visibility visibility;
+
+    public enum Visibility {
+        PUBLIC, PRIVATE
+    }
 }
